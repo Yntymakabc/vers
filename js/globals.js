3eddfda839687ff60d562ea8ecf91be5f4861344
Azamat Mercury <111359649+zhanbol1@users.noreply.github.com>
2025-06-14 17:19:22 +0600

    payment1

--- js/globals.js
+++ js/globals.js
@@ -3,7 +3,8 @@ window.currentUser = {
     username: localStorage.getItem('username') || null,
     balance: localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 0.0,
     courses: [],
-    isLoggedIn: !!localStorage.getItem('username')
+    isLoggedIn: !!localStorage.getItem('username'),
+    id: localStorage.getItem('id') || null
 };
 
 // Helper to update global user info from localStorage
@@ -11,6 +12,7 @@ window.updateCurrentUser = function() {
     window.currentUser.username = localStorage.getItem('username') || null;
     window.currentUser.balance = localStorage.getItem('balance') ? parseFloat(localStorage.getItem('balance')) : 0.0;
     window.currentUser.isLoggedIn = !!window.currentUser.username;
+    window.currentUser.id = localStorage.getItem('id') || null;
     // You can add logic to load courses if you store them in localStorage
     // window.currentUser.courses = JSON.parse(localStorage.getItem('courses') || '[]');
 };
